8c34a5d00849c08d78213727b071d004347186a7
Srinivas92 <srinivasaperumals@tadconsultancy.com>
2016-11-17 01:25:19 +0530

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject hiccup-templating "0.1.0-SNAPSHOT"
+  :description "Hiccup examples for Clojure Cookbook"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [hiccup "1.0.4"]])
